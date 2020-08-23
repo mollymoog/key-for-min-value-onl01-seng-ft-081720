4347fcf685f59d1306b92b6dfe2274ba9f36dc78
molly mk <mmkover@gmail.com>
2020-08-23 01:00:04 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,9 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
+  small = []
+  name_hash.collect do |item, value|
+    small << value
+  end
+  smallest_value = small.sort.shift
+  name_hash.invert[smallest_value]
 
 end
